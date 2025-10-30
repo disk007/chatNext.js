9cc8396ae02375e2ec18852fc9f3d7a3486aff0e
disk007 <disc1314@gmail.com>
2025-10-30 22:55:39 +0700

    updateJoin

--- src/app/components/validation/validation.ts
+++ src/app/components/validation/validation.ts
@@ -1,10 +1,22 @@
 import {z} from 'zod';
 
 export const AddRoomSchema = z.object({
-  room: z.string().min(1, {message:'Chat Room is required.'}),
+  room: z.string().min(1, {message:'Chat Room is required.'})
+    .refine((val) => !/^\s/.test(val), { message: "Chat Room cannot start with a space." }),
 });
 export const AddMessageSchema = z.object({
-  message: z.string().min(1, {message:'Message is required.'}),
+  message: z
+    .string()
+    .min(1, {message:'Message is required.'})
+    .refine((val) => !/^\s/.test(val), { message: "Message cannot start with a space." }),
+
+});
+export const JoinRoomSchema = z.object({
+  code: z.string().min(1, {message: "Code is required." })
+    .refine((val) => !/^\s/.test(val), {
+      message: "Code cannot start with a space.",
+    }),
 });
 export type AddRoomData = z.infer<typeof AddRoomSchema>;
 export type AddMessageData = z.infer<typeof AddMessageSchema>;
+export type JoinRoomData = z.infer<typeof JoinRoomSchema>;
